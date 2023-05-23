f7edbcc89f1d829a44169f8b61292bed87ddee99
Salman Hadli <skh174211@gmail.com>
2023-05-23 19:46:12 +0530

    add functionality to show greeting in the form of Toast

--- app/src/main/java/com/example/greetings/MainActivity.java
+++ app/src/main/java/com/example/greetings/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.greetings;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         heading = findViewById(R.id.header);
         nameInput = findViewById(R.id.nameInput);
         btn = findViewById(R.id.btn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameInput.getText().toString();
+                String message;
+                System.out.println(name);
+                if (name.length() > 0) {
+                    message = "Hello " + name;
+                } else {
+                    message = "Please Enter your name!";
+                }
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
